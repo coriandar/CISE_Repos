b719ede4c88cec4677cd9f3ca72df2d0847ff2d9
coriandar <88608040+coriandar@users.noreply.github.com>
2024-07-21 22:59:19 +1200

    test[work02]: add vitest with examples

---
+++ worksheet02/vite-frontend/src/App.test.ts
@@ -0,0 +1,17 @@
+import { expect, test } from "vitest";
+
+export function sum(a: number, b: number): number {
+    return a + b;
+}
+
+test("knows that 2 and 2 make 4", () => {
+    expect(2 + 2).toBe(4);
+});
+
+test("adds 1 + 2 to equal 3", () => {
+    expect(sum(1, 2)).toBe(3);
+});
+
+test("adds 1 + 5 to equal 6", () => {
+    expect(sum(1, 1)).toBe(6);
+});
