39d52f1038dd7c314ec2f6249e0b8332dfae7ca9
coriandar <88608040+coriandar@users.noreply.github.com>
2024-08-04 01:35:11 +1200

    [work03]: add template strings

--- worksheet03/frontend/src/components/ShowBookDetails.tsx
+++ worksheet03/frontend/src/components/ShowBookDetails.tsx
@@ -87,12 +87,8 @@ export default function ShowBookDetails() {
                     </div>
                     <br />
                     <div className="col-md-8 m-auto">
-                        <h1 className="display-4 text-center">
-                            Book&quot;s Record
-                        </h1>
-                        <p className="lead text-center">
-                            View Book&quot;s Info
-                        </p>
+                        <h1 className="display-4 text-center">{`Book's Record`}</h1>
+                        <p className="lead text-center">{`View Book's Info`}</p>
                         <hr /> <br />
                     </div>
                     <div className="col-md-10 m-auto">{BookItem}</div>
